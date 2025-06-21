6e84b9b9ca34ea2932caf997b1c3cb57a0004018
M-Sanjay12o52o <msnjy12o52o@gmail.com>
2025-06-21 10:56:22 +0530

    define and register function schemas to enable Gemini tool use

--- functions/schemas/schema_write_file.py
+++ functions/schemas/schema_write_file.py
@@ -1,1 +1,20 @@
+from google.genai import types
 
+schema_write_file = types.FunctionDeclaration(
+  name="write_file",
+  description="The path to the file to write, relative to the working directory. Example: 'main.txt'.",
+  parameters=types.Schema(
+    type=types.Type.OBJECT,
+    properties={
+      "file_path": types.Schema(
+        type=types.Type.STRING,
+        description="The directory where the file will be written, relative to the working directory. If not provided, defaults to the working directory itself.",
+      ),
+      "content": types.Schema(
+        type=types.Type.STRING,
+        description="The content to write to the file. If the file already exists, it will be overwritten.",
+      )
+    },
+    required=["file_path", "content"]
+  ),
+)
--- functions/tool_schemas.py
+++ functions/tool_schemas.py
@@ -1,9 +1,15 @@
 # functions/tool_schemas.py
 from google.genai import types
 from functions.schemas.schema_get_files_info import schema_get_files_info
+from functions.schemas.schema_run_python_file import schema_run_python
+from functions.schemas.schema_write_file import schema_write_file
+from functions.schemas.schema_get_file_content import schema_get_file_content
 
 available_function = types.Tool(
   function_declarations=[
-    schema_get_files_info
+    schema_get_files_info,
+    schema_run_python,
+    schema_write_file,
+    schema_get_file_content,
   ]
 )
